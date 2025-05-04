423b9be41dfff6efca4128db94b09d520b44cdc1
Akio Kane <aakocherga@mail.ru>
2025-05-04 20:10:58 +0700

    create base structure

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+
+const indexRouter = require("./routes/indexRouter");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+const assetsPathPublic = path.join(__dirname, "public");
+const assetsPathAssets = path.join(__dirname, "assets");
+const assetsPathFonts = path.join(__dirname, "fonts");
+const assetsPathDOM = path.join(__dirname, "DOM");
+
+app.use("/public", express.static(assetsPathPublic));
+app.use("/assets", express.static(assetsPathAssets));
+app.use("/fonts", express.static(assetsPathFonts));
+app.use("/DOM", express.static(assetsPathDOM));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs")
+
+app.use("/", indexRouter)
+
+app.use((req, res, next) => {
+  res.status(404).render("404", {
+    title: "Page not found!", 
+    text: "Try returning to the home page..."
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("app listen in", PORT);
+})
